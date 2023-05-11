fb29a8d9203d035fd3ffc7fdd26e9b01f9b550ba
Marcelo Rigoni <marcrigoni@gmail.com>
2023-05-11 17:53:12 -0300

    First controller and Razor pages tests

---
+++ Vendas/Controllers/Departments/DepartmentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Vendas.Models;
+
+namespace Vendas.Controllers.Departments
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
---
+++ Vendas/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace Vendas.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
